850f790e48b546c442ec4b9797942f2088033e72
HHumbert <Will@Athena.local>
2014-09-08 04:42:52 +0800

    Initial Commit

---
+++ UITableViewController Challenge/CC_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CC_AppDelegate.h
+//  UITableViewController Challenge
+//
+//  Created by William Wagers on 9/8/14.
+//  Copyright (c) 2014 William Wagers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CC_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge/CC_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  CC_ViewController.h
+//  UITableViewController Challenge
+//
+//  Created by William Wagers on 9/8/14.
+//  Copyright (c) 2014 William Wagers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CC_ViewController : UIViewController
+
+@end
